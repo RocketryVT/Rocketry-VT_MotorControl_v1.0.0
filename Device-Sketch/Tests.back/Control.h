937d888e6d3b4c35c460cede2fef5e777e7463ea
MattMarti <matthew.marti@gmail.com>
2018-10-31 16:53:05 -0400

    Removed duplicated test folder

--- Device-Sketch/Tests.back/Control.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "stdafx.h"
-
-// Things not included in Hardware.h but that I need access to
-extern unsigned long START_TIME;
-extern unsigned long TIME;
-unsigned long micros();
--- Device-Sketch/Tests.back/Test_Transmission.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int TestTransmission();
-bool Test_0x51();
-bool Test_0x52();
-bool Test_0x10();
