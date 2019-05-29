7a65fbd4e50847686e2486699144dc54f0d1843e
Wade Foster <jwade109@vt.edu>
2019-05-29 01:20:55 +0000

    Add gpio demo

---
+++ gpio/gpio.hpp
@@ -0,0 +1,40 @@
+#ifndef RVT_GPIO_HPP
+#define RVT_GPIO_HPP
+
+#include <string>
+
+namespace gpio
+{
+
+int led_set_value(unsigned int led, bool value);
+
+int export_gpio(unsigned int gpio);
+
+int unexport_gpio(unsigned int gpio);
+
+int set_dir(unsigned int gpio, bool out_flag);
+
+int set_value(unsigned int gpio, bool value);
+
+int get_value(unsigned int gpio, unsigned int &value);
+
+int set_edge(unsigned int gpio, std::string &edge);
+
+int fd_open(unsigned int gpio);
+
+int fd_close(int fd);
+
+} // namespace gpio
+
+namespace adc
+{
+
+int read_value(unsigned int adc, unsigned int &value);
+
+int fd_open(unsigned int adc);
+
+int fd_close(int fd);
+
+} // namespace adc
+
+#endif /* RVT_GPIO_HPP */
